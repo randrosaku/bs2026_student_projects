bab638d48e99ad63c36aadd0864573512b311ca9
Juta Varnyte <varnytejuta@gmail.com>
2026-04-16 17:45:30 +0300

    delete unused import

--- Juta_Varnyte/dashboard.py
+++ Juta_Varnyte/dashboard.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-import numpy_financial as npf
 from windrose import WindroseAxes
 
 st.set_page_config(layout="wide")
--- README.md
+++
@@ -1,2 +0,0 @@
-# bs2026_student_projects
-Your final project codebase and a pdf report has to be push to this repo 
